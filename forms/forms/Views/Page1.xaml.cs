931f3da1ccaf357bac75c7b45a1f5442a59b0445
Gabriel <jcorn@GABO>
2024-06-25 16:00:26 -0500

    Agregar archivos de proyecto.

---
+++ forms/forms/Views/Page1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace forms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void EnviarCV(object sender, EventArgs e)
+        {
+            string nombre = nombre_.Text;
+            string apellido = apellido_.Text;
+            string email = email_.Text;
+            string telefono = numero_.Text;
+            string experiencia = experencia_.Text;
+            string habilidades = habilidades_.Text;
+
+
+            DisplayAlert("Información", "Datos registrados.", "Cerrar");
+
+        }
+    }
+}
